021717771f8f792a3a15c70e55815714291cdcff
THURIKA R <thurikavrc@gmail.com>
2026-05-30 10:13:29 +0530

    Add files via upload

---
+++ Core-Java/DataType.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class DataType {
+    public static void main(String[] args) {
+
+        int age = 20;
+        float pi = 3.14f;
+        double salary = 45000.75;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name="Thurika";
+
+        System.out.println("Integer value: " + age);
+        System.out.println("Float value: " + pi);
+        System.out.println("Double value: " + salary);
+        System.out.println("Character value: " + grade);
+        System.out.println("Boolean value: " + isJavaFun);
+        System.out.println("String value: " + name);
+    }
+}
